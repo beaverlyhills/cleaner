3a10c198e625ceee26c0917a4d84528501628d3c
Denis Khromov <beaverlyhills@gmail.com>
2017-05-28 18:18:38 -0700

    Initial code with file and image hashes

---
+++ main.go
@@ -0,0 +1,223 @@
+package main
+
+import (
+	"bufio"
+	"crypto/sha1"
+	"encoding/binary"
+	"encoding/hex"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"image"
+	"image/jpeg"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/rwcarlsen/goexif/exif"
+)
+
+var dbFile string
+
+// FileMetadata contains cached file metadata
+type FileMetadata struct {
+	Path      string
+	Size      int64
+	FileHash  string
+	ImageHash string
+	Modified  time.Time
+	DateShot  time.Time
+}
+
+func visitFunc(dbFile string, files map[string]*FileMetadata, hashes map[string][]*FileMetadata) filepath.WalkFunc {
+	return func(path string, f os.FileInfo, err error) error {
+		if f.IsDir() {
+			return nil
+		}
+		record := files[path]
+		if record != nil {
+			if f.Size() == record.Size && f.ModTime() == record.Modified && len(record.FileHash) > 0 {
+				return nil
+			}
+			fmt.Printf("Metadata changed for %s\n", path)
+		}
+		fileHash, err := getFileHash(path)
+		if err != nil {
+			return err
+		}
+		imageHash, err := getImageHash(path)
+		if err != nil {
+			fmt.Printf("Not an image %s\n", path)
+		}
+		dateShot, err := getImageDate(path)
+		if err != nil {
+			fmt.Printf("No exif %s\n", path)
+		}
+		record = &FileMetadata{Path: path, Modified: f.ModTime(), Size: f.Size(), FileHash: fileHash, ImageHash: imageHash, DateShot: dateShot}
+		addRecord(files, hashes, record)
+		addFileToDB(dbFile, record)
+		fmt.Printf("Added %s\n", path)
+		return nil
+	}
+}
+
+func addFileToDB(dbFile string, record *FileMetadata) error {
+	file, err := os.OpenFile(dbFile, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	data, err := json.Marshal(record)
+	if err != nil {
+		return err
+	}
+	if _, err = file.Write(data); err != nil {
+		return err
+	}
+	if _, err = file.WriteString("\n"); err != nil {
+		return err
+	}
+	fmt.Printf("Saved metadata for %s\n", record.Path)
+	return nil
+}
+
+func getFileHash(path string) (string, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+	hasher := sha1.New()
+	if _, err := io.Copy(hasher, f); err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(hasher.Sum(nil)), nil
+}
+
+func getImageHash(path string) (string, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+	fmt.Printf("Reading image %s\n", path)
+	image, err := jpeg.Decode(f)
+	if err != nil {
+		return "", err
+	}
+	hasher := sha1.New()
+	if err := writeImage(hasher, image); err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(hasher.Sum(nil)), nil
+}
+
+func getImageDate(path string) (time.Time, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return time.Time{}, err
+	}
+	defer f.Close()
+	fmt.Printf("Reading exif %s\n", path)
+	x, err := exif.Decode(f)
+	if err != nil {
+		return time.Time{}, err
+	}
+	datetime, err := x.DateTime() // DateTime, DateTimeOriginal
+	if err == nil {
+		return datetime, nil
+	}
+	// Try also DateTimeDigitized, GPSTimeStamp
+	return time.Time{}, err
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func writeImage(writer io.Writer, image image.Image) error {
+	size := image.Bounds().Dx() * image.Bounds().Dy()
+	width := image.Bounds().Dx()
+	for x := 0; x < size; x++ {
+		row := x / width
+		col := x - row*width
+		c := image.At(col, row)
+		r, g, b, a := c.RGBA()
+		if err := binary.Write(writer, binary.LittleEndian, r); err != nil {
+			return err
+		}
+		if err := binary.Write(writer, binary.LittleEndian, g); err != nil {
+			return err
+		}
+		if err := binary.Write(writer, binary.LittleEndian, b); err != nil {
+			return err
+		}
+		if err := binary.Write(writer, binary.LittleEndian, a); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func readDB(path string) (map[string]*FileMetadata, map[string][]*FileMetadata, error) {
+	files := make(map[string]*FileMetadata)
+	hashes := make(map[string][]*FileMetadata)
+	file, err := os.Open(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return files, hashes, nil
+		}
+		return nil, nil, err
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		var record FileMetadata
+		err := json.Unmarshal(scanner.Bytes(), &record)
+		if err != nil {
+			return nil, nil, err
+		}
+		if len(record.Path) > 0 {
+			fmt.Printf("Restored metadata for %s\n", record.Path)
+			addRecord(files, hashes, &record)
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, nil, err
+	}
+	return files, hashes, nil
+}
+
+func addRecord(files map[string]*FileMetadata, hashes map[string][]*FileMetadata, record *FileMetadata) {
+	files[record.Path] = record
+	hashes[record.FileHash] = append(hashes[record.FileHash], record)
+	if len(record.ImageHash) > 0 {
+		hashes[record.ImageHash] = append(hashes[record.ImageHash], record)
+	}
+}
+
+func main() {
+	flag.StringVar(&dbFile, "db", "cache.txt", "Database file path, default value is cache.txt")
+	flag.Parse()
+	files, hashes, err := readDB(dbFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, path := range flag.Args() {
+		fmt.Printf("Scanning %s\n", path)
+		err := filepath.Walk(path, visitFunc(dbFile, files, hashes))
+		fmt.Printf("filepath.Walk() returned %v\n", err)
+	}
+}
