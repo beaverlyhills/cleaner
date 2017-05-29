03a51e0b8ba52d5a563bb1f8f912276417a31204
Denis Khromov <beaverlyhills@gmail.com>
2017-05-29 12:21:49 -0700

    Read moov timestamp, handle zero timestamps in EXIF

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"encoding/binary"
 	"encoding/hex"
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"image"
@@ -14,12 +15,14 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 	"time"
 
-	"github.com/rwcarlsen/goexif/exif"
-)
+	"s.mcquay.me/sm/mov"
 
-var dbFile string
+	"github.com/rwcarlsen/goexif/exif"
+	"github.com/rwcarlsen/goexif/tiff"
+)
 
 // FileMetadata contains cached file metadata
 type FileMetadata struct {
@@ -42,6 +45,7 @@ func visitFunc(dbFile string, files map[string]*FileMetadata, hashes map[string]
 				return nil
 			}
 			fmt.Printf("Metadata changed for %s\n", path)
+			removeRecord(files, hashes, record)
 		}
 		fileHash, err := getFileHash(path)
 		if err != nil {
@@ -51,16 +55,25 @@ func visitFunc(dbFile string, files map[string]*FileMetadata, hashes map[string]
 		if err != nil {
 			fmt.Printf("Not an image %s\n", path)
 		}
-		dateShot, err := getImageDate(path)
-		if err != nil {
-			fmt.Printf("No exif %s\n", path)
-		}
+		dateShot, err := getMediaDate(path)
 		record = &FileMetadata{Path: path, Modified: f.ModTime(), Size: f.Size(), FileHash: fileHash, ImageHash: imageHash, DateShot: dateShot}
 		addRecord(files, hashes, record)
 		addFileToDB(dbFile, record)
 		fmt.Printf("Added %s\n", path)
 		return nil
 	}
+}
+
+func getMediaDate(path string) (time.Time, error) {
+	dateShot, err := getImageDate(path)
+	if err != nil {
+		fmt.Printf("No exif %s\n", path)
+		dateShot, err = getMovieDate(path)
+		if err != nil {
+			fmt.Printf("No moov %s\n", path)
+		}
+	}
+	return dateShot, err
 }
 
 func addFileToDB(dbFile string, record *FileMetadata) error {
@@ -125,11 +138,50 @@ func getImageDate(path string) (time.Time, error) {
 	if err != nil {
 		return time.Time{}, err
 	}
-	datetime, err := x.DateTime() // DateTime, DateTimeOriginal
+	datetime, err := getOriginalDateTime(x) // DateTime, DateTimeOriginal
 	if err == nil {
 		return datetime, nil
 	}
-	// Try also DateTimeDigitized, GPSTimeStamp
+	return time.Time{}, err
+}
+
+func getOriginalDateTime(x *exif.Exif) (time.Time, error) {
+	dt, err := getDateTimeFromTag(exif.DateTimeOriginal, x)
+	if err != nil {
+		dt, err = getDateTimeFromTag(exif.DateTimeDigitized, x)
+		if err != nil {
+			dt, err = getDateTimeFromTag(exif.DateTime, x)
+		}
+	}
+	return dt, err
+}
+
+func getDateTimeFromTag(name exif.FieldName, x *exif.Exif) (time.Time, error) {
+	tag, err := x.Get(name)
+	if err != nil {
+		return time.Time{}, err
+	}
+	if tag.Format() != tiff.StringVal {
+		return time.Time{}, errors.New("DateTime[Original] not in string format")
+	}
+	exifTimeLayout := "2006:01:02 15:04:05"
+	dateStr := strings.TrimRight(string(tag.Val), "\x00")
+	// TODO(bradfitz,mpl): look for timezone offset, GPS time, etc.
+	// For now, just always return the time.Local timezone.
+	return time.ParseInLocation(exifTimeLayout, dateStr, time.Local)
+}
+
+func getMovieDate(path string) (time.Time, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return time.Time{}, err
+	}
+	defer f.Close()
+	fmt.Printf("Reading moov %s\n", path)
+	datetime, err := mov.Created(f)
+	if err == nil {
+		return datetime, nil
+	}
 	return time.Time{}, err
 }
 
@@ -148,8 +200,8 @@ func max(x, y int) int {
 }
 
 func writeImage(writer io.Writer, image image.Image) error {
-	size := image.Bounds().Dx() * image.Bounds().Dy()
 	width := image.Bounds().Dx()
+	size := width * image.Bounds().Dy()
 	for x := 0; x < size; x++ {
 		row := x / width
 		col := x - row*width
@@ -172,6 +224,7 @@ func writeImage(writer io.Writer, image image.Image) error {
 }
 
 func readDB(path string) (map[string]*FileMetadata, map[string][]*FileMetadata, error) {
+	fmt.Printf("Reading database from %s\n", path)
 	files := make(map[string]*FileMetadata)
 	hashes := make(map[string][]*FileMetadata)
 	file, err := os.Open(path)
@@ -208,7 +261,25 @@ func addRecord(files map[string]*FileMetadata, hashes map[string][]*FileMetadata
 	}
 }
 
+func removeRecord(files map[string]*FileMetadata, hashes map[string][]*FileMetadata, record *FileMetadata) {
+	delete(files, record.Path)
+	hashes[record.FileHash] = deleteRecord(hashes[record.FileHash], record)
+	if len(record.ImageHash) > 0 {
+		hashes[record.ImageHash] = deleteRecord(hashes[record.ImageHash], record)
+	}
+}
+
+func deleteRecord(records []*FileMetadata, record *FileMetadata) []*FileMetadata {
+	for i, r := range records {
+		if r.Path == record.Path {
+			return append(records[:i], records[i+1:]...)
+		}
+	}
+	return records
+}
+
 func main() {
+	var dbFile string
 	flag.StringVar(&dbFile, "db", "cache.txt", "Database file path, default value is cache.txt")
 	flag.Parse()
 	files, hashes, err := readDB(dbFile)
@@ -218,6 +289,9 @@ func main() {
 	for _, path := range flag.Args() {
 		fmt.Printf("Scanning %s\n", path)
 		err := filepath.Walk(path, visitFunc(dbFile, files, hashes))
-		fmt.Printf("filepath.Walk() returned %v\n", err)
+		if err != nil {
+			fmt.Printf("filepath.Walk() returned %v\n", err)
+		}
+		fmt.Printf("Finished scanning\n")
 	}
 }
